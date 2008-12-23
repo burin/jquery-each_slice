f89056b9d75b2a7403c2eef983c54416c4986b53
brandonaaron <brandon.aaron@gmail.com>
2008-12-23 11:16:36 -0600

    jQuery each_slice plugin

---
+++ jquery.each_slice.js
@@ -0,0 +1,16 @@
+/*! Copyright (c) 2008 Burin Asavesna (http://helloburin) and Brandon Aaron (http://brandonaaron.net)
+ * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) 
+ * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
+ */
+
+jQuery.fn.each_slice = function(num) {
+	for ( var i=0,count=0,slice=[]; i<=this.length; i++ ) {
+		if ( count >= num ) {
+			fn.apply( jQuery( slice ) );
+			count = 0, slice=[];
+		}
+		slice.push( this[i] );
+		count++;
+	}
+	return this;
+};
